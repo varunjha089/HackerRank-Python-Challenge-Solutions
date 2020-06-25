0cbf484b659f8de69ba17ddd07008c11538b172b
VARUN KUMAR <varunjha089@gmail.com>
2020-06-25 20:49:32 +0530

    I am capable to find the string.

---
+++ Find-a-string/Find-a-string.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+    occurrence = 0
+
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i + len(sub_string)] == sub_string:
+            occurrence += 1
+
+    return occurrence
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
